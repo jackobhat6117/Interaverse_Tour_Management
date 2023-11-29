b4e31fa3e1a5bacebaee8ac346fc0b94965e46a1
Natnael Mekonnen <nhattymack204@gmail.com>
2023-11-29 23:37:44 +0300

    feature(finance): add bank account

---
+++ src/controllers/settings/finance/addBankAccount.js
@@ -0,0 +1,24 @@
+import fetchServer from "../../fetchServer";
+
+export default async function addBankAccount(data) {
+  let result = {
+    return: 0,
+    msg: "Something went wrong adding bank account!",
+  };
+
+  await fetchServer({
+    method: "POST",
+    url: "/payment/v1/payment/addBankAccount",
+    data,
+  })
+    .then((res) => {
+      if (res?.data && !res?.data?.error) {
+        result = { return: 1, msg: "Successful", data: res?.data?.data };
+      } else if (res?.data?.error) result["msg"] = res.data.error;
+    })
+    .catch((err) => {
+      console.log("Network Error!");
+    });
+
+  return result;
+}
