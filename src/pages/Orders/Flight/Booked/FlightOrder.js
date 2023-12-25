9da6bad1227bc30fd718320e381973525fddb18b
Natnael Mekonnen <nhattymack204@gmail.com>
2023-12-25 18:20:45 +0300

    feature(flightOrder): setup flight details fetch

--- src/pages/Orders/Flight/Booked/FlightOrder.js
+++ src/pages/Orders/Flight/Booked/FlightOrder.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react'
-import BreadCrumb from '../../../../components/DIsplay/Nav/BreadCrumb'
-import { Link } from 'react-router-dom'
-import Button1 from '../../../../components/form/Button1'
-import TextInput from '../../../../components/form/TextInput'
-import { Menu } from '../../OrdersData'
-import Modal1 from '../../../../components/DIsplay/Modal/Modal1'
-import RadioGroup from '../../../../components/form/RadioGroup'
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
-import FlightDoc from './FlightDoc'
-import Insurance from './Insurance'
-import EmailExport from './EmailExport'
-import StatusBar from './StatusBar'
-import ShareViaEmail from './SareViaEmail'
-import PriceSummary from './PriceSummary'
-import PassengerInfo from './PassengerInfo'
-import FlightInfo from './FlightInfo'
-
+import React, { useEffect, useState } from "react";
+import BreadCrumb from "../../../../components/DIsplay/Nav/BreadCrumb";
+import { Link, useSearchParams } from "react-router-dom";
+import Button1 from "../../../../components/form/Button1";
+import TextInput from "../../../../components/form/TextInput";
+import { Menu } from "../../OrdersData";
+import Modal1 from "../../../../components/DIsplay/Modal/Modal1";
+import RadioGroup from "../../../../components/form/RadioGroup";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import FlightDoc from "./FlightDoc";
+import Insurance from "./Insurance";
+import EmailExport from "./EmailExport";
+import StatusBar from "./StatusBar";
+import ShareViaEmail from "./SareViaEmail";
+import PriceSummary from "./PriceSummary";
+import PassengerInfo from "./PassengerInfo";
+import FlightInfo from "./FlightInfo";
+import { useParams } from "react-router-dom";
+import getBooking from "../../../../controllers/booking/getBooking";
 
 export default function FlightOrder() {
   let obj = {
     needsReview: true,
-    orderId: 'F34FJJ6'
-  }
-  const [onChangedData,setOnChangedData] = useState(true);
-  const [openExport,setOpenExport] = useState(false);
-  const [openEmailExport,setOpenEmailExport] = useState(false);
-  const [openPDFExport,setOpenPDFExport] = useState(false);
-  const [openInsurance,setOpenInsurance] = useState(false);
-  
-  const [selectedOption,setSelectedOption] = useState();
-
+    orderId: "F34FJJ6",
+  };
+  const { id } = useParams();
+  const [onChangedData, setOnChangedData] = useState(true);
+  const [openExport, setOpenExport] = useState(false);
+  const [openEmailExport, setOpenEmailExport] = useState(false);
+  const [openPDFExport, setOpenPDFExport] = useState(false);
+  const [openInsurance, setOpenInsurance] = useState(false);
+  const [selectedOption, setSelectedOption] = useState();
+  const [order, setOrder] = useState();
 
   function handleOption() {
-    if(selectedOption === 'email')
-      setOpenEmailExport(true);
-    else if(selectedOption === 'pdf') {
+    if (selectedOption === "email") setOpenEmailExport(true);
+    else if (selectedOption === "pdf") {
       setOpenPDFExport(true);
-      setTimeout(() => handlePDFExport(),2000);
+      setTimeout(() => handlePDFExport(), 2000);
     }
 
     setOpenExport(false);
@@ -45,11 +45,11 @@ export default function FlightOrder() {
 
   function handlePDFExport() {
     // Get the component element
-    const component = document.getElementById('flightDoc');
+    const component = document.getElementById("flightDoc");
 
     // Capture screenshot using html2canvas
     html2canvas(component).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
+      const imgData = canvas.toDataURL("image/png");
 
       // Create a new jsPDF instance
       const pdf = new jsPDF();
@@ -59,80 +59,137 @@ export default function FlightOrder() {
       const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
 
       // Add the screenshot image to the PDF document
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
 
       // Download the PDF
-      pdf.save(obj?.orderId+'.pdf');
+      pdf.save(obj?.orderId + ".pdf");
     });
   }
 
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getBooking(id);
+      if (res.return) {
+        setOrder(res.data);
+      }
+    };
+    fetch();
+  }, [id]);
+
+  console.log(order);
+
   return (
-    <div className='flex flex-col gap-4 pd-md py-4'>
+    <div className="flex flex-col gap-4 pd-md py-4">
       <BreadCrumb>
-        <Link to='/order' >Orders</Link>
+        <Link to="/order">Orders</Link>
         <label>Confirmation</label>
       </BreadCrumb>
 
-      <div className='flex gap-4'>
-        <div className='flex flex-col gap-4 flex-1'>
-          <div className='flex gap-4 items-center'>
+      <div className="flex gap-4">
+        <div className="flex flex-col gap-4 flex-1">
+          <div className="flex gap-4 items-center">
             <h5>Flight details</h5>
 
-            <div className='flex flex-1 gap-3 justify-end flex-wrap'>
+            <div className="flex flex-1 gap-3 justify-end flex-wrap">
               <div>
-                <Button1 variant={'outlined'} className=''
+                <Button1
+                  variant={"outlined"}
+                  className=""
                   onClick={() => setOpenExport(true)}
-                >Export itinerary</Button1>
+                >
+                  Export itinerary
+                </Button1>
               </div>
-              <div >
-                <TextInput select size='small' label='Manage this order' noShrink className='!min-w-[180px] bg-primary/10'>
-                  <div className='menuItem'>
-                    <Menu value={'pending'} label='Add Seats' hideFor={['confirmed']} />
-                    <Menu value={'pending'} label='Add Bags' hideFor={['confirmed']} />
-                    <Menu value={'pending'} label='Add Insurance' hideFor={['confirmed']} onClick={() => setOpenInsurance(true)} />
-                    <Menu value={'pending'} label='Confirm Payment' showFor={['pending','on hold']} />
-                    <Menu value={'pending'} label='Edit PNR' hideFor={['confirmed']} />
-                    <Menu value={'pending'} label='Hold Order' hideFor={['confirmed']} />
-                    <Menu value={'pending'} label='Cancel Order' className='!bg-red-500 !text-white !rounded-md' />
+              <div>
+                <TextInput
+                  select
+                  size="small"
+                  label="Manage this order"
+                  noShrink
+                  className="!min-w-[180px] bg-primary/10"
+                >
+                  <div className="menuItem">
+                    <Menu
+                      value={"pending"}
+                      label="Add Seats"
+                      hideFor={["confirmed"]}
+                    />
+                    <Menu
+                      value={"pending"}
+                      label="Add Bags"
+                      hideFor={["confirmed"]}
+                    />
+                    <Menu
+                      value={"pending"}
+                      label="Add Insurance"
+                      hideFor={["confirmed"]}
+                      onClick={() => setOpenInsurance(true)}
+                    />
+                    <Menu
+                      value={"pending"}
+                      label="Confirm Payment"
+                      showFor={["pending", "on hold"]}
+                    />
+                    <Menu
+                      value={"pending"}
+                      label="Edit PNR"
+                      hideFor={["confirmed"]}
+                    />
+                    <Menu
+                      value={"pending"}
+                      label="Hold Order"
+                      hideFor={["confirmed"]}
+                    />
+                    <Menu
+                      value={"pending"}
+                      label="Cancel Order"
+                      className="!bg-red-500 !text-white !rounded-md"
+                    />
                   </div>
                 </TextInput>
               </div>
             </div>
           </div>
-          
+
           <hr />
-          {obj?.needsReview ? 
-            <div className='flex flex-col gap-4'>
-              <div className='flex gap-4 items-center'>
-                <div className='flex flex-1 justify-start gap-2 '>
-                  <div className=''>
-                    <button className={`${onChangedData ? '!btn':'btn-light'}`}
+          {obj?.needsReview ? (
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-4 items-center">
+                <div className="flex flex-1 justify-start gap-2 ">
+                  <div className="">
+                    <button
+                      className={`${onChangedData ? "!btn" : "btn-light"}`}
                       onClick={() => setOnChangedData(true)}
-                    >New Details</button>
+                    >
+                      New Details
+                    </button>
                   </div>
-                  <div className=''>
-                    <button className={`${!onChangedData ? '!btn':'btn-light'}`}
+                  <div className="">
+                    <button
+                      className={`${!onChangedData ? "!btn" : "btn-light"}`}
                       onClick={() => setOnChangedData(false)}
-                    >Previous Details</button>
+                    >
+                      Previous Details
+                    </button>
                   </div>
                 </div>
-                <button className='warn text-sm px-4 font-normal'>Changes detected</button>
+                <button className="warn text-sm px-4 font-normal">
+                  Changes detected
+                </button>
               </div>
               <hr />
             </div>
-          :null}
+          ) : null}
 
           <FlightInfo />
-          <div className='py-4'>
-            <PassengerInfo label={'Adult'} />
-            <PassengerInfo label={'Child'} />
+          <div className="py-4">
+            <PassengerInfo label={"Adult"} />
+            <PassengerInfo label={"Child"} />
           </div>
           <PriceSummary />
           <ShareViaEmail />
-
         </div>
 
-        
         <div>
           <StatusBar data={obj} />
         </div>
@@ -143,17 +200,29 @@ export default function FlightOrder() {
       </Modal1>
 
       <Modal1 open={openExport} setOpen={setOpenExport}>
-        <div className='card p-10 flex flex-col gap-4'>
-          <h5 className='self-center'>Export Order</h5>
-          <RadioGroup options={[
-            {title: 'Export to Email',description: 'Select the option to export the order via email',value: 'email'},
-            {title: 'Export as PDF',description: 'Select the option to export the order via PDF',value: 'pdf'},
-          ]} className='flex flex-col gap-4' radioClass='!items-start' render={(obj) => (
-            <div className='flex flex-col '>
-              <b>{obj.title}</b>
-              <p>{obj.description}</p>
-            </div>
-          )} 
+        <div className="card p-10 flex flex-col gap-4">
+          <h5 className="self-center">Export Order</h5>
+          <RadioGroup
+            options={[
+              {
+                title: "Export to Email",
+                description: "Select the option to export the order via email",
+                value: "email",
+              },
+              {
+                title: "Export as PDF",
+                description: "Select the option to export the order via PDF",
+                value: "pdf",
+              },
+            ]}
+            className="flex flex-col gap-4"
+            radioClass="!items-start"
+            render={(obj) => (
+              <div className="flex flex-col ">
+                <b>{obj.title}</b>
+                <p>{obj.description}</p>
+              </div>
+            )}
             value={selectedOption}
             onChange={(val) => setSelectedOption(val)}
           />
@@ -170,5 +239,5 @@ export default function FlightOrder() {
         <Insurance cancel={() => setOpenInsurance(false)} />
       </Modal1>
     </div>
-  )
+  );
 }
