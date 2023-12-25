da3a079230831b52020938c4b3c09b3363ea8188
Yeabsira <abebey348@gmail.com>
2023-12-25 14:51:28 +0300

    payment method ui update

--- src/components/flight/PaymentMethod.js
+++ src/components/flight/PaymentMethod.js
@@ -6,9 +6,10 @@ import payForTicket from "../../controllers/Flight/payForTicket";
 import { useSnackbar } from "notistack";
 import { useState } from "react";
 
-export default function PaymentMethod({data,className}) {
+export default function PaymentMethod({data,callback,className}) {
   // const {id} = useParams();
   // Bank, Card, USSD, QR, MobileMoney, BankTransfer
+  const [paynow,setPaynow] = useState(false);
   const options = [
     {icon: <Icon icon='' />,name: 'Pay with stored card',description: 'Pay with stored card',value: 'stored'},
     {icon: <Icon icon='' />,name: 'Pay instantly with a new card',description: 'Pay with a debit or credit card',value: 'Card'},
@@ -36,17 +37,27 @@ export default function PaymentMethod({data,className}) {
     <div className={'border p-4 flex flex-col gap-6 md:min-w-[400px] '+className}>
       <h5>Choose payment method</h5>
       <hr />
-      <RadioGroup options={options} value={method} 
-        className='flex flex-col gap-4'
-        onChange={(val) => setMethod(val)}
-        render={(obj) => (
-          <div className="flex flex-col">
-            <span>{obj.name}</span>
-            <small>{obj.description}</small>
-          </div>
-        )} />
-      <hr />
-      <Button1 loading={loading} onClick={handlePay}>Pay now</Button1>
+      {paynow ? 
+        <div className="flex flex-col gap-3">
+          <RadioGroup options={options} value={method} 
+            className='flex flex-col gap-4'
+            onChange={(val) => setMethod(val)}
+            render={(obj) => (
+              <div className="flex flex-col">
+                <span>{obj.name}</span>
+                <small>{obj.description}</small>
+              </div>
+            )} />
+          <hr />
+          <Button1 loading={loading} onClick={handlePay}>Pay now</Button1>
+          <button className="btn" onClick={() => callback && callback()}>Hold booking and pay later</button>
+        </div>
+      :
+        <div className="flex flex-col gap-3">
+          <Button1 onClick={() => setPaynow(true)}>Pay now</Button1>
+          <button className="btn" onClick={() => callback && callback()}>Hold booking and pay later</button>
+        </div>
+      }
     </div>
   )
 }
