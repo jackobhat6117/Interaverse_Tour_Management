c600dd9af6a092e78e9dccc6aca1cf1793b6edd7
Natnael Mekonnen <nhattymack204@gmail.com>
2024-01-17 15:45:17 +0300

    feature(Markup): create arkup

--- src/components/form/AirlinesInput.js
+++ src/components/form/AirlinesInput.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
-import { InputAdornment, TextField } from '@mui/material';
-import FetcherInput from '../mini/FetcherInput';
-import getAirlineCodes from '../../controllers/Flight/getAirlines';
+import React, { useState } from "react";
+import { InputAdornment, TextField } from "@mui/material";
+import FetcherInput from "../mini/FetcherInput";
+import getAirlineCodes from "../../controllers/Flight/getAirlines";
 
-export default function AirlinesInput({val,returnData,option,label,icon,multiple}) {
-  const [ariline,setAriline] = useState([]);
-  const [data,setData] = useState(val || "")
+export default function AirlinesInput({
+  val,
+  returnData,
+  option,
+  label,
+  icon,
+  multiple,
+}) {
+  const [ariline, setAriline] = useState([]);
+  const [data, setData] = useState(val || "");
 
   // option?.map(val => {
   //   let airlines = [];
@@ -15,26 +22,23 @@ export default function AirlinesInput({val,returnData,option,label,icon,multiple
   //   console.log('----------')
   //   return console.log(airlines)
   // })
-  
+
   async function handleChange(val) {
-    if(!val || val.length === 0)
-      return returnData && returnData(null)
+    if (!val || val.length === 0) return returnData && returnData(null);
     getAriline(val);
     setData(val);
   }
   function handleReturn(val) {
     // console.log(' ---',val)
-    if(returnData)
-      returnData(val);
+    if (returnData) returnData(val);
   }
-  
-  async function getAriline(val,callback) {
-    if(val === '') return false;
+
+  async function getAriline(val, callback) {
+    if (val === "") return false;
     const res = await getAirlineCodes(val);
-    if(res.return)
-      if(!callback)
-        setAriline(res.data?.data);
-      else callback(res.data?.data)
+    if (res.return)
+      if (!callback) setAriline(res.data?.data);
+      else callback(res.data?.data);
   }
 
   const filterOptions = (options, inputValue) => {
@@ -42,45 +46,58 @@ export default function AirlinesInput({val,returnData,option,label,icon,multiple
     return options.filter(
       (option) =>
         option.name.toLowerCase().startsWith(inputValueLowerCase) ||
-        option.id.toLowerCase().startsWith(inputValueLowerCase)
+        option.id.toLowerCase().startsWith(inputValueLowerCase),
     );
   };
 
-
   return (
-    <FetcherInput className='min-w-[200px]'
+    <FetcherInput
+      className="min-w-[200px]"
       options={ariline}
       getOptionLabel={(option) =>
-        typeof option === 'string' ? option : option.name
+        typeof option === "string" ? option : option.name
       }
-      filterOptions={(options,{inputValue}) => filterOptions(options,inputValue)}
-      value={data} size='small'
-      onInputChange={(ev,newVal) => handleChange(newVal)}
-      onChange={(ev,newVal) => handleReturn(newVal)}
+      filterOptions={(options, { inputValue }) =>
+        filterOptions(options, inputValue)
+      }
+      value={data}
+      size="small"
+      onInputChange={(ev, newVal) => handleChange(newVal)}
+      onChange={(ev, newVal) => handleReturn(newVal)}
       // multiple
       renderInput={(params) => (
-        <TextField {...params} label={label}
+        <TextField
+          {...params}
+          label={label}
           InputProps={{
             ...params.InputProps,
-            type: 'search',
+            type: "search",
             startAdornment: (
-              <InputAdornment position='start'>
-                {icon}
-              </InputAdornment>
-            )
+              <InputAdornment position="start">{icon}</InputAdornment>
+            ),
           }}
           InputLabelProps={{
             shrink: true,
-          }} />
+          }}
+        />
       )}
-      renderOption={(props,opt) => {
+      renderOption={(props, opt) => {
         return (
-          <div {...props} className='flex flex-col !p-2 !cursor-pointer' style={{padding: 10,cursor: 'pointer'}}>
+          <div
+            {...props}
+            className="flex flex-col !p-2 !cursor-pointer"
+            style={{ padding: 10, cursor: "pointer" }}
+          >
             <h4>{opt.id}</h4>
-            <small className='!whitespace-nowrap !text-ellipsis !block' title={opt.name}>{opt.name}</small>
+            <small
+              className="!whitespace-nowrap !text-ellipsis !block"
+              title={opt.name}
+            >
+              {opt.name}
+            </small>
           </div>
-        )
+        );
       }}
     />
-  )
+  );
 }
