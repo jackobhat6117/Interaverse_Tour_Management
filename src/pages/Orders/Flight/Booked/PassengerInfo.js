69adb77a5b5126a8a27c58c30ca9a64428632b86
Yeabsira <abebey348@gmail.com>
2024-04-23 17:25:16 +0300

    more passenger data on order details page

--- src/pages/Orders/Flight/Booked/PassengerInfo.js
+++ src/pages/Orders/Flight/Booked/PassengerInfo.js
@@ -1,10 +1,11 @@
 import Icon from "../../../../components/HOC/Icon";
 import Button1 from "../../../../components/form/Button1";
+import CopyText from "../../../../components/mini/CopyText";
 import { toPascalCase } from "../../../../utils/toPascal";
 
 
 export default function PassengerInfo({ label, traveler,order }) {
-  
+  console.log(traveler)
   return (
     <div className="flex flex-col gap-2 py-4">
       <div className="flex gap-6 flex-wrap overflow-x-auto whitespace-nowrap max-w-full">
@@ -13,26 +14,98 @@ export default function PassengerInfo({ label, traveler,order }) {
             <Icon icon="ic:person" />1 {label}
           </Button1>
         </div>
-        <table className="w-[500px]" >
-          <thead>
-            <td className='pr-4'>Name</td>
-            <td className='px-4'>Date of birth</td>
-            <td className='px-4'>Gender</td>
-            <td className='px-4'>{traveler?.document?.documentType} Id</td>
-          </thead>
-          <tbody>
-            <td className='pr-4'>
-              {traveler?.firstName || traveler?.name?.firstName} {traveler?.lastName || traveler?.name?.lastName}
-            </td>
-            <td className='px-4'>{traveler?.birthDate}</td>
-            <td className='px-4'>{traveler?.gender ? toPascalCase(traveler?.gender) : ""}</td>
-            <td className='px-4'>{traveler?.document?.number}</td>
-          </tbody>
-        </table>
+        <div className="flex flex-col gap-6">
+          <table className="w-[500px]" >
+            <thead>
+              <td className='pr-4'>Name</td>
+              <td className='px-4'>Date of birth</td>
+              <td className='px-4'>Email</td>
+            </thead>
+            <tbody>
+              <td className='pr-4'>
+                <Copier value={`${traveler?.firstName || traveler?.name?.firstName} ${traveler?.lastName || traveler?.name?.lastName}`} className={'w-[100px]'} />
+              </td>
+              <td className='px-4'>
+                <Copier value={traveler?.birthDate} className={'w-[100px]'} />
+              </td>
+              <td className='px-4'>
+                <Copier value={`${traveler?.email}`}  />
+              </td>
+            </tbody>
+          </table>
+
+
+          <table className="w-[500px]" >
+            <thead>
+              <td className='pr-4'>Gender</td>
+              <td className='px-4'>Phone</td>
+              <td className='px-4'>{traveler?.document?.documentType} Id</td>
+            </thead>
+            <tbody>
+              <td className='pr-4'>
+                <Copier value={traveler?.gender ? toPascalCase(traveler?.gender) : ""} className={'w-[100px]'}  />
+              </td>
+              <td className='px-4'>
+                <Copier value={traveler?.phone} className={'w-[100px]'} />
+              </td>
+              <td className='px-4'><Copier value={traveler?.document?.number}  /></td>
+            </tbody>
+          </table>
+
+
+          <table className="w-[500px]" >
+            <thead>
+              <td className='pr-4'>Birth Place</td>
+              <td className='px-4'>Document expiry</td>
+              <td className='px-4'>Issuance Date</td>
+              <td className='px-4'>Issuance Country</td>
+            </thead>
+            <tbody>
+              <td className='pr-4'>
+                <Copier value={`${traveler?.document?.birthPlace}`} className={'w-[100px]'} />
+              </td>
+              <td className='px-4'>
+                <Copier value={traveler?.document?.expiryDate} className={'w-[100px]'} />
+              </td>
+              <td className='px-4'><Copier value={traveler?.document?.issuanceDate} className={'w-[100px]'} /></td>
+              <td className='px-4'>
+                <Copier value={traveler?.document?.birthPlace} className={'w-[100px]'} />
+              </td>
+            </tbody>
+          </table>
+
+
+          <table className="w-[500px]" >
+            <thead>
+              <td className='pr-4'>Issuance Location</td>
+              <td className='px-4'>Nationality</td>
+              <td className='px-4'>Validity Country</td>
+            </thead>
+            <tbody>
+              <td className='pr-4'>
+                <Copier value={`${traveler?.document?.birthPlace}`} className={'w-[100px]'} />
+              </td>
+              <td className='px-4'>
+                <Copier value={traveler?.document?.expiryDate} className={'w-[100px]'} />
+              </td>
+              <td className='px-4'>
+                <Copier value={traveler?.document?.birthPlace}  />
+              </td>
+            </tbody>
+          </table>
+
+          
+        </div>
       </div>
       {/* <div>
         <FlightInfo minify data={order}/>
       </div> */}
     </div>
   );
+}
+
+function Copier({value,className}) {
+  return (
+    <CopyText><input type='text' className={className} value={value} /></CopyText>
+  )
 }
