bb6c4d27a764a1c0f0f8cbba6691d0c7c9479dcd
jackobhat911@gmail.com <jackobhat911@gmail.com>
2024-10-05 11:07:15 +0300

    adding insurance features

---
+++ src/pages/Settings/Fees/Insurance.js
@@ -0,0 +1,44 @@
+import React, { useState , useEffect } from 'react';
+import CardList from '../../../components/Settings/fees/FlightCard';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SingleSelectDropdown from '../../../components/Settings/fees/SingleDropDown';
+
+
+const Insurance = () => {
+
+
+  const cardData1 = [
+    { title: 'Confirmed booking fee', amount: '$534,000', btn: 'save'},
+    { title: 'Cancellation fee', amount: '$534,000', btn: 'save' },
+    { title: 'Refund fee', amount: '$534,000', btn: 'save' },
+    { title: 'Change fee', amount: '$534,000', btn: 'save' }
+  ];
+
+
+  return (
+    <div className="container mx-auto py-5">
+        <div>
+        <div className="mt-5">
+          <Accordion expanded= {true}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              <h5>Tours Booking</h5>
+            </AccordionSummary>
+            <AccordionDetails>
+              <CardList cards={cardData1} />
+            </AccordionDetails>
+          </Accordion>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Insurance;
